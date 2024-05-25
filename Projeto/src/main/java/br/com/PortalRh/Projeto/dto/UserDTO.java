56504c877762c0c3fa786769121095ccd22efff2
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-05-25 16:15:17 -0300

    feat: criados DTOs

---
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/dto/UserDTO.java
@@ -0,0 +1,38 @@
+package br.com.PortalRh.Projeto.dto;
+
+public class UserDTO {
+    private String username;
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
