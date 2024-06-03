4fa5ec92b40aec9557c1039f0066c9a9e8a912ae
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-06-03 09:49:43 -0300

    fix: excluido import não usado

--- Projeto/src/main/java/br/com/PortalRh/Projeto/model/Address.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/model/Address.java
@@ -4,7 +4,6 @@ import br.com.PortalRh.Projeto.model.enums.State;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
