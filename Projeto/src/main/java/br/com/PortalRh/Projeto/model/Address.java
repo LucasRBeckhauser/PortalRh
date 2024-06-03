df2d8f875dc9ecdea338e6512162b331e4a5d9b2
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-06-03 10:29:22 -0300

    fix: removido import

--- Projeto/src/main/java/br/com/PortalRh/Projeto/model/Address.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/model/Address.java
@@ -4,6 +4,7 @@ import br.com.PortalRh.Projeto.model.enums.State;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
