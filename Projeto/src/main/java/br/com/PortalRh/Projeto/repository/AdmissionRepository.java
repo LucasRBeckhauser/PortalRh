0b1a35851d6671af128195722f73377a3875314c
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-06-04 20:13:49 -0300

    fix: excluidas todas as  repositories, services e controllers pois não condiziam com as entidades do projeto e é mais simples refazer do que refatorar

--- Projeto/src/main/java/br/com/PortalRh/Projeto/repository/AdmissionRepository.java
+++
@@ -1,9 +0,0 @@
-//package br.com.PortalRh.Projeto.repository;
-//
-//import br.com.PortalRh.Projeto.entities.Admission;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface AdmissionRepository extends JpaRepository <Admission, Long > {
-//}
--- Projeto/src/main/java/br/com/PortalRh/Projeto/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.PortalRh.Projeto.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.PortalRh.Projeto.model.Person;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
