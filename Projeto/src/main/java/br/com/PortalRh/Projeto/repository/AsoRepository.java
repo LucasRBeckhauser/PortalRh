74d5d85ba7c106d1034b7c927786d4c8f832d033
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-06-04 20:45:10 -0300

    feat: criados repositories

---
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/repository/AsoRepository.java
@@ -0,0 +1,7 @@
+package br.com.PortalRh.Projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.PortalRh.Projeto.model.Aso;
+
+public interface AsoRepository extends JpaRepository<Aso, Long> {}
---
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/repository/CollaboratorRepository.java
@@ -0,0 +1,7 @@
+package br.com.PortalRh.Projeto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.PortalRh.Projeto.model.Collaborator;
+
+public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {}
