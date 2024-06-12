8fb49ff94c94e103253bc2fc8728b09c013505c7
bruno.jucoski <brunojucoskibr@hotmail.com>
2024-06-12 20:11:39 -0300

    plural controllers request mapping

--- Projeto/src/main/java/br/com/PortalRh/Projeto/controller/PersonController.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/person")
+@RequestMapping("api/persons")
 public class PersonController {
     @Autowired
     private PersonService personService;
