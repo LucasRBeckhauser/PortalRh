7a1e53a048054788afa666bbd5f8df8f6ffc658e
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-06-15 01:38:16 -0300

    Implementação do jakarta validation. classes Addres, Admission, BankData, Certification, Clhildren e Collaborator (Dtos)

--- Projeto/src/main/java/br/com/PortalRh/Projeto/model/dtos/CertificationDTO.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/model/dtos/CertificationDTO.java
@@ -3,9 +3,15 @@ package br.com.PortalRh.Projeto.model.dtos;
 import br.com.PortalRh.Projeto.model.Person;
 
 public record CertificationDTO(
-    String title,
-    String skills,
-    Integer workload,
-    String certificateDescription,
-    Person person
+        @NotBlank(message = "O título não pode estar em branco")
+        String title,
+        @NotBlank(message = "As habilidades não podem estar em branco")
+        String skills,
+        @NotNull(message = "A carga horária de trabalho não pode ser nula.")
+        Integer workload,
+        @NotBlank(message = "A descrição não pode estar em branco.")
+        String certificateDescription,
+        @Valid
+        @NotNull(message = "A pessoa não pode ser nula")
+        Person person
 ) {}
--- Projeto/src/main/java/br/com/PortalRh/Projeto/model/dtos/CollaboratorDTO.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/model/dtos/CollaboratorDTO.java
@@ -7,11 +7,19 @@ import br.com.PortalRh.Projeto.model.enums.ContractModality;
 import br.com.PortalRh.Projeto.model.enums.Shift;
 
 public record CollaboratorDTO(
-    double wage,
-    String pisPasep,
-    ContractModality contractModality,
-    Shift shift,
-    JobPosition jobPosition,
-    Admission admission,
-    Demission demission
+        @NotNull(message = "A remuneração não pode ser nula")
+        double wage,
+        @NotNull(message = "Pis é campo obrigatório")
+        String pisPasep,
+
+        @Valid @NotNull(message = "O contrato não pode ser nulo")
+        ContractModality contractModality,
+        @Valid @NotNull(message = "O turno não pode ser nulo")
+        Shift shift,
+        @Valid @NotNull(message = "O cargo não pode ser nulo")
+        JobPosition jobPosition,
+        @Valid @NotNull(message = "A admissão não pode ser nula")
+        Admission admission,
+        @Valid @NotNull(message = "A demissão não pode ser nula")
+        Demission demission
 ) {}
