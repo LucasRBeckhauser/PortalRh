a59ced74f0c1523c98f39edc03b50f58a2386985
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-06-14 23:03:07 -0300

    fix: adicionados validations nos dtos

--- Projeto/src/main/java/br/com/PortalRh/Projeto/model/dtos/SocialMediaDTO.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/model/dtos/SocialMediaDTO.java
@@ -1,9 +1,18 @@
 package br.com.PortalRh.Projeto.model.dtos;
 
+import org.hibernate.validator.constraints.URL;
+
 import br.com.PortalRh.Projeto.model.Person;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SocialMediaDTO(
+    @NotBlank(message = "O nome da organização não pode estar em branco") 
     String name,
+    
+    @URL(message = "A URL deve ser válida") 
     String url,
+    
+    @NotNull(message = "A pessoa não pode ser nula") 
     Person person
 ) {}
