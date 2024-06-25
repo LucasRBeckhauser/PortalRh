d7434b161df7d47a634608f85ddfc33d1aeddb09
Bruno Jucoski <brunojucoskibr@hotmail.com>
2024-06-25 18:38:23 -0300

    Iniciando implementação nas DTO's

--- Projeto/src/main/java/br/com/PortalRh/Projeto/controller/dtos/BankDataDTO.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/controller/dtos/BankDataDTO.java
@@ -5,15 +5,69 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record BankDataDTO(
-    @Valid
-    @NotNull(message = "O tipo da conta não pode ser nula.")
-    AccountType accountType,
-    @Valid
-    @NotBlank(message = "O banco não pode estar em branco")
-    String bank,
-    @NotBlank(message = "A agência não pode estar em branco")
-    String agency,
-    @NotBlank(message = "A conta não pode estar em branco")
-    String account
-) {}
+public class BankDataDTO {
+    AccountType accountType;
+    String bank;
+    String agency;
+    String account;
+
+
+    public BankDataDTO() {
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getAgency() {
+        return agency;
+    }
+
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public BankDataDTO(AccountType accountType, String bank, String agency, String account) {
+        this.accountType = accountType;
+        this.bank = bank;
+        this.agency = agency;
+        this.account = account;
+
+
+
+
+    }
+}
+
+
+//    @Valid
+//    @NotNull(message = "O tipo da conta não pode ser nula.")
+//    AccountType accountType,
+//    @Valid
+//    @NotBlank(message = "O banco não pode estar em branco")
+//    String bank,
+//    @NotBlank(message = "A agência não pode estar em branco")
+//    String agency,
+//    @NotBlank(message = "A conta não pode estar em branco")
+//    String account
+//) {}
--- Projeto/src/main/java/br/com/PortalRh/Projeto/controller/dtos/CertificationDTO.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/controller/dtos/CertificationDTO.java
@@ -5,16 +5,74 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record CertificationDTO(
-        @NotBlank(message = "O título não pode estar em branco")
-        String title,
-        @NotBlank(message = "As habilidades não podem estar em branco")
-        String skills,
-        @NotNull(message = "A carga horária de trabalho não pode ser nula.")
-        Integer workload,
-        @NotBlank(message = "A descrição não pode estar em branco.")
-        String certificateDescription,
-        @Valid
-        @NotNull(message = "A pessoa não pode ser nula")
-        Person person
-) {}
+public class CertificationDTO{
+        String title;
+        String skills;
+        Integer workload;
+        String certificateDescription;
+        Person person;
+
+        public CertificationDTO() {
+        }
+
+        public CertificationDTO(String title, String skills, Integer workload, String certificateDescription, Person person) {
+                this.title = title;
+                this.skills = skills;
+                this.workload = workload;
+                this.certificateDescription = certificateDescription;
+                this.person = person;
+        }
+
+
+        public String getTitle() {
+                return title;
+        }
+
+        public void setTitle(String title) {
+                this.title = title;
+        }
+
+        public String getSkills() {
+                return skills;
+        }
+
+        public void setSkills(String skills) {
+                this.skills = skills;
+        }
+
+        public Integer getWorkload() {
+                return workload;
+        }
+
+        public void setWorkload(Integer workload) {
+                this.workload = workload;
+        }
+
+        public String getCertificateDescription() {
+                return certificateDescription;
+        }
+
+        public void setCertificateDescription(String certificateDescription) {
+                this.certificateDescription = certificateDescription;
+        }
+
+        public Person getPerson() {
+                return person;
+        }
+
+        public void setPerson(Person person) {
+                this.person = person;
+        }
+}
+//        @NotBlank(message = "O título não pode estar em branco")
+//        String title,
+//        @NotBlank(message = "As habilidades não podem estar em branco")
+//        String skills,
+//        @NotNull(message = "A carga horária de trabalho não pode ser nula.")
+//        Integer workload,
+//        @NotBlank(message = "A descrição não pode estar em branco.")
+//        String certificateDescription,
+//        @Valid
+//        @NotNull(message = "A pessoa não pode ser nula")
+//        Person person
+//) {}
