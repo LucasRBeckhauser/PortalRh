065c06ffbf936b92d4f2f93988067bb73cc07d5a
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-06-26 18:37:28 -0300

    Correção da User no Dto.

--- Projeto/src/main/java/br/com/PortalRh/Projeto/controller/dtos/UserDTO.java
+++ Projeto/src/main/java/br/com/PortalRh/Projeto/controller/dtos/UserDTO.java
@@ -1,13 +1,65 @@
 package br.com.PortalRh.Projeto.controller.dtos;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import br.com.PortalRh.Projeto.model.User;
 
-public record UserDTO(
-    @NotBlank(message = "O nome de usuário não pode estar em branco") 
-    String username,
-    
-    @NotBlank(message = "A senha não pode estar em branco") 
-    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres") 
-    String password
-) {}
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO{
+    String username;
+    String password;
+
+    public UserDTO() {}
+
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public static UserDTO fromEntity(User user) {
+        return new UserDTO(
+                user.getPassword(),
+                user.getUsername()
+        );
+    }
+    public static List<UserDTO> fromEntityList(List<User> users){
+        List<UserDTO> userDTOList = new ArrayList<>();
+        for (User user : users) {
+            userDTOList.add(fromEntity(user));
+        }
+        return userDTOList;
+    }
+
+    public User toEntity() {
+        User user = new User();
+        user.setPassword(this.password);
+        user.setUsername(this.username);
+
+        return user;
+    }
+
+    public static List<User> toEntityList(List<UserDTO> userDTOS) {
+        List<User> users = new ArrayList<>();
+        for (UserDTO userDTO : userDTOS) {
+            users.add(userDTO.toEntity());
+        }
+        return users;
+    }
+
+}
